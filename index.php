3271a1f0b950dad969d7d89ef68111475bcb7b50
Luke McKennan <luke.mckennan@gmail.com>
2015-12-11 14:12:16 +0800

    [controllers] scopes - global & modules / arrays / asyncronous scopes

--- index.php
+++ index.php
@@ -29,16 +29,82 @@
 		Last name: <input placeholder="type something" type="text" ng-model="lastName" /><br />
 		Hello {{firstName}} {{lastName}} -->
 
-		controller<br />Initializing the model using an Angular controller defined in an Angular module<br /><br />
+<!-- 		controller<br />Initializing the model using an Angular controller defined in an Angular module<br /><br />
 		First name: <input placeholder="type something" type="text" ng-model="firstName" /><br />
 		Last name: <input placeholder="type something" type="text" ng-model="lastName" /><br />
-		Hello {{firstName}} {{lastName}}
+		Hello {{firstName}} {{lastName}} -->
+
+<!-- 		controller<br />Changing scope values asynchronously - updates dont propagate without .apply()<br /><br />
+		First name: <input placeholder="type something" type="text" ng-model="firstName" /><br />
+		Last name: <input placeholder="type something" type="text" ng-model="lastName" /><br />
+		Hello {{firstName}} {{lastName}} -->
+
+		Controller Arrays<br />Looping over lists in templates using ng-repeat<br /><br />
+		<ul>
+			<li ng-repeat="name in names">{{name}}
+			<!-- the a tag is within the nested scope thus 'name' is callable -->
+			<a href="" ng-click="removeName(name)">remove</a></li>
+		</ul>
+
+		<form ng-submit="addName()">
+			<input type="text" ng-model="enteredName" />
+			<input type="submit" value="Add" />
+		</form>
 	</div>
 	<script>
+		// // controller
+		// var nameApp = angular.module('nameApp', []);
+		// nameApp.controller('NameCtrl', function ($scope) {
+		// 	$scope.firstName = 'John';
+		// 	$scope.lastName = 'Smith';
+		// });
+
+		// // changing scope values
+		// var nameApp = angular.module('nameApp', []);
+		// nameApp.controller('NameCtrl', function ($scope) {
+		// 	$scope.firstName = 'John';
+		// 	// $watch listens to changes of specific property in the model
+		// 	$scope.$watch('lastName', function(newValue, oldValue) {
+		// 		// immediately calls for value - currently undefined so how to tell Angular things changed?
+		// 		console.log('new value is ' + newValue);
+		// 		// $watch is kind of like getting outside of the Angular "world"
+		// 		// ie run custom JS in response to model
+		// 	});
+		// 	setTimeout(function() {
+		// 		$scope.lastName = 'Smith';
+		// 		// $apply() tells Angular to apply changes
+		// 		// we need to manually propagate changes because we are ouside the Angular "world"
+		// 		// this is an asynchronous event
+		// 		$scope.$apply();
+		// 		// use Angular's own setTimeout feature to auto propagate changes
+		// 	}, 1000);
+		// });
+
+		// // Controller Arrays
+		// var nameApp = angular.module('nameApp', []);
+		// nameApp.controller('NameCtrl', function ($scope) {
+		// 	$scope.names = ['Larry', 'Curly', 'Moe'];
+		// });
+
+		// Controller Arrays
 		var nameApp = angular.module('nameApp', []);
 		nameApp.controller('NameCtrl', function ($scope) {
-			$scope.firstName = 'John';
-			$scope.lastName = 'Smith';
+			$scope.names = ['Larry', 'Curly', 'Moe'];
+
+			// declare addName() to $scope for <form> to give data to
+			$scope.addName = function () {
+				// enteredName is bind to the <input> text field
+				$scope.names.push($scope.enteredName);
+				
+				// to clear input field after adding
+				$scope.enteredName = "";
+			};
+
+			$scope.removeName = function (name) {
+				var i = $scope.names.indexOf(name);
+				// .splice ("what you want to find", 0=add/1=remove/2=remove and keep ongoing array)
+				$scope.names.splice(i, 1);
+			};
 		});
 	</script>
 </body>
